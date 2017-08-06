d47169b41e0f72afa0e9a79902e97a89bd02e1c5
sham6ss <sham6ss2657@gmail.com>
2017-08-06 15:00:22 +0900

    初めてのコミット

---
+++ kadai5-2_ko.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	for (int num = 127;num > 31;num -= 16)//numを127に設定：num=32に達するまで一周ごとに16減る
+	{
+		for (int i = num; i > num - 16;i--)
+			printf("%2c  |", i);//numから1引きながら対応する文字を表示
+		printf("\n");//改行
+		for (int i = num;i > num - 16;i--)
+			printf("%3d |", i);//numから1引きながら引いた結果の数字を表示
+		printf("\n");//改行
+		for (int i = 0;i < 80;i++)
+			printf("=");//=を80個並べる
+		printf("\n");//改行
+	}
+	return 0;
+}
